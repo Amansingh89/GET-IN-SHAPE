ef47be27c790e46b955a0130003b7002c970f98d
Aman Singh <Aman@singh.org>
2016-12-01 15:06:27 +0000

    Configured Django for Heroku

--- fitnessproject/settings.py
+++ fitnessproject/settings.py
@@ -23,10 +23,8 @@ SITE_ID = 1
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -48,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'paypal.standard.ipn',
     'magazines',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -87,13 +86,13 @@ WSGI_APPLICATION = 'fitnessproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 #
-# CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
-# DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
+CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -142,13 +141,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # static directory at the project level
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # static directory at the project level
 
 DISQUS_WEBSITE_SHORTNAME = 'mybootcampblog'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 SITE_URL = 'http://127.0.0.1:8000'
 PAYPAL_NOTIFY_URL = 'http://127.0.0.1/a-very-hard-to-guess-url/'
@@ -164,7 +163,33 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
 
+DEBUG = os.getenv('DJANGO_DEBUG')
 
-# ALLOWED_HOSTS = ['get-in-shape.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['get-in-shape.herokuapp.com', '127.0.0.1']
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 
 
+# # STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+# )
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
