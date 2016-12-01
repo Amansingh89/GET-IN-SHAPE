a3b54b69c14f280c8db0478aa4cbbf26479b871f
Aman Singh <Aman@singh.org>
2016-12-01 21:00:04 +0000

    register

--- fitnessproject/settings.py
+++ fitnessproject/settings.py
@@ -86,13 +86,13 @@ WSGI_APPLICATION = 'fitnessproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 #
-CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
-DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
+# CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
+# DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
